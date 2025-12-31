bd5c35d4194e204516ffdf4bb8b888d38bbffe27
PedroAugSouza <pedrosouza.contatodev@gmail.com>
2025-12-31 16:23:52 -0300

    add command to generate

--- src/services/chat/index.ts
+++ src/services/chat/index.ts
@@ -6,7 +6,7 @@ import {
 } from "./contracts";
 
 const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL
-  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  ? `${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : "http://localhost:3000";
 
 export const chatService = {
