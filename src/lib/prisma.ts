a55c5931ad3d7461036afcf140407a4077afb332
PedroAugSouza <pedrosouza.contatodev@gmail.com>
2025-12-31 15:56:25 -0300

    add command to generate

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.POSTGRES_PRISMA_URL!,
+  connectionString: process.env.POSTGRES_PRISMA_URL!.split("?")[0],
 });
 
 export const prisma = new PrismaClient({ adapter });
